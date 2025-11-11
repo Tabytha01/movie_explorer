0a7277439943d780e4dac6f4fc63a5bcaaa1840d
Tabytha01 <kunda.tabitha@gmail.com>
2025-11-11 16:03:25 +0200

    debugging

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 // App layout: navbar + routed pages
 // Routes are defined inside <Routes> and rendered in <main>
 import { Routes, Route } from 'react-router-dom'
-import './App.css'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import MovieDetails from './pages/MovieDetails'
